38997a41fbad641f120399b904da4e37d66170dd
Peter Thomas <ptrthomas@gmail.com>
2023-06-07 14:10:21 +0530

    improve code and readme

--- quarkus/src/test/java/karate/GreetingTest.java
+++ quarkus/src/test/java/karate/GreetingTest.java
@@ -7,20 +7,15 @@ import io.quarkus.test.junit.QuarkusTest;
 import java.net.URL;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @QuarkusTest
 class GreetingTest {
-    
-    private static final Logger logger = LoggerFactory.getLogger(GreetingTest.class); 
 
     @TestHTTPResource("/")
     URL urlBase;
     
     @Test
     void testGreeting() {
-        logger.debug("url base is: {}", urlBase);
         Results results = Runner.path("classpath:karate/greeting.feature")
                 .systemProperty("url.base", urlBase.toString())
                 .parallel(1);
