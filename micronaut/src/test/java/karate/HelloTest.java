d61f10d334bcfe85612a256a8180e1070cee97fd
Peter Thomas <ptrthomas@gmail.com>
2023-07-12 11:51:19 +0530

    test does not need to be public

--- micronaut/src/test/java/karate/HelloTest.java
+++ micronaut/src/test/java/karate/HelloTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 @MicronautTest
-public class HelloTest {
+class HelloTest {
     
     @Inject
     EmbeddedServer server;    
